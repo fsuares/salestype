6e2a5b41fb5cb55d2c459adf15266c6afe10441e
Fernando Suares <fernandoaqs@outlook.com.br>
2024-09-25 15:07:40 -0300

    feat: add show product service

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/errors/AppError';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+
+interface IRequest {
+    id: string;
+}
+
+class ShowProductService {
+    public async execute({ id }: IRequest): Promise<Product> {
+        const productsRepository = getCustomRepository(ProductsRepository);
+        const product = await productsRepository.findOne(id);
+        if (!product) {
+            throw new AppError('Product not found', 404);
+        }
+        return product;
+    }
+}
+
+export default ShowProductService;
