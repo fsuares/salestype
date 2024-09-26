ff08f1cf2e69995db3b2adcd83481be8d6d33162
Fernando Suares <fernandoaqs@outlook.com.br>
2024-09-26 20:36:48 -0300

    feat: add validations to products router

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,19 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/database';
+import { errors } from 'celebrate';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
+
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof AppError) {
         return res.status(error.statusCode).json({
