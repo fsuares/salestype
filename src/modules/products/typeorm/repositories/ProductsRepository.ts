bba2d08b033b9dfcc87e8530be7a189351d63fba
Fernando Suares <fernandoaqs@outlook.com.br>
2024-10-01 14:49:30 -0300

    feat: Add association between Product and OrdersProducts entities

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,5 +1,9 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import Product from '@products/typeorm/entities/Product';
+
+interface IFindProducts {
+    product_id: string;
+}
 
 @EntityRepository(Product)
 export class ProductsRepository extends Repository<Product> {
@@ -9,5 +13,16 @@ export class ProductsRepository extends Repository<Product> {
                 name
             }
         });
+    }
+
+    public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+        const productIds = products.map((product) => product.product_id);
+        const productsExists = await this.find({
+            where: {
+                id: In(productIds)
+            }
+        });
+
+        return productsExists;
     }
 }
